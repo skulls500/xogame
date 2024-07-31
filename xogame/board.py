b1f2e23f2391de71dba712066a886b28069afd0c
Yoav Kleinberger <haarcuba@gmail.com>
2024-07-31 18:52:02 +0300

    refactoring
    refactoring === changing code without changing behaviour

--- xogame/board.py
+++ xogame/board.py
@@ -3,19 +3,21 @@ class Board:
         self._board = {key: None for key in 'abcdefghi'}
 
     def winner(self):
-        for row in ('abc', 'def', 'ghi'):
-            if self._board[row[0]] == self._board[row[1]] == self._board[row[2]]:
-                return self._board[row[0]]
+        WINNING_COMBINATIONS = (
+            'abc', 'def', 'ghi',
+            'adg', 'beh', 'cfi',
+            'aei', 'ceg'
+        )
 
-        for column in ('adg', 'beh', 'cfi'):
-            if self._board[column[0]] == self._board[column[1]] == self._board[column[2]]:
-                return self._board[column[0]]
+        for combination in WINNING_COMBINATIONS:
+            if self._same_symbol(combination):
+                return self._board[combination[0]]
 
-        if self._board['a'] == self._board['e'] == self._board['i']:
-            return self._board['a']
+        return None
 
-        if self._board['c'] == self._board['e'] == self._board['g']:
-            return self._board['c']
+    def _same_symbol(self, combination):
+        first = self._board[combination[0]]
+        return all(self._board[key] == first for key in combination)
 
     def place(self, symbol, position):
         self._board[position] = symbol
