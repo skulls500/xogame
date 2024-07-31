266ffb41921820622d80dee9c41b5924ef3d7b26
Yoav Kleinberger <haarcuba@gmail.com>
2024-07-31 18:41:37 +0300

    basic game done

--- tests/test_board.py
+++ tests/test_board.py
@@ -3,3 +3,51 @@ import xogame.board
 def test_empty_board_has_no_winner():
     board = xogame.board.Board()
     assert board.winner() is None
+
+# a b c
+# d e f
+# g h i
+
+def test_board_with_three_X_in_top_row_has_winner_X():
+    board = xogame.board.Board()
+    board.place('X', 'a')
+    board.place('X', 'b')
+    assert board.winner() is None
+    board.place('X', 'c')
+    assert board.winner() == 'X'
+
+def test_board_game_with_X_and_O__O_wins_on_major_diagonal():
+    board = xogame.board.Board()
+    board.place('X', 'b')
+    board.place('O', 'a')
+    assert board.winner() is None
+
+    board.place('X', 'c')
+    board.place('O', 'e')
+    assert board.winner() is None
+
+    board.place('X', 'f')
+    board.place('O', 'i')
+    assert board.winner() == 'O'
+
+def test_board_with_three_X_in_left_column():
+    board = xogame.board.Board()
+    board.place('X', 'a')
+    board.place('X', 'd')
+    assert board.winner() is None
+    board.place('X', 'g')
+    assert board.winner() == 'X'
+
+def test_board_game_with_X_and_O__O_wins_on_minor_diagonal():
+    board = xogame.board.Board()
+    board.place('X', 'b')
+    board.place('O', 'c')
+    assert board.winner() is None
+
+    board.place('X', 'd')
+    board.place('O', 'e')
+    assert board.winner() is None
+
+    board.place('X', 'f')
+    board.place('O', 'g')
+    assert board.winner() == 'O'
--- xogame/board.py
+++ xogame/board.py
@@ -1,3 +1,21 @@
 class Board:
+    def __init__(self):
+        self._board = {key: None for key in 'abcdefghi'}
+
     def winner(self):
-        return None
+        for row in ('abc', 'def', 'ghi'):
+            if self._board[row[0]] == self._board[row[1]] == self._board[row[2]]:
+                return self._board[row[0]]
+
+        for column in ('adg', 'beh', 'cfi'):
+            if self._board[column[0]] == self._board[column[1]] == self._board[column[2]]:
+                return self._board[column[0]]
+
+        if self._board['a'] == self._board['e'] == self._board['i']:
+            return self._board['a']
+
+        if self._board['c'] == self._board['e'] == self._board['g']:
+            return self._board['c']
+
+    def place(self, symbol, position):
+        self._board[position] = symbol
